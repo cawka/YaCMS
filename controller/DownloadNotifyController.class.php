5efa0e2098224494557c5ac1aeb56883fb4fe371
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2011-07-22 16:11:15 -0700

    Update Download tracking controllers. Small update with search keys

--- controller/DownloadNotifyController.class.php
+++ controller/DownloadNotifyController.class.php
@@ -4,6 +4,8 @@ class DownloadNotifyController extends DownloadController
 {
 	protected function track( $file, $info )
 	{
+		new GAHelper( $SETTINGS['ga.account'], $file );
+
 		new SearchKeywordEmailerHelper( );
 		return parent::track( $file, $info ); // not necessary, but just in case we need to do something there
 	}
